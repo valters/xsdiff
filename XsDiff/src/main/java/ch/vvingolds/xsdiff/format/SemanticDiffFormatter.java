61a484f6cc07aaa69c65ac113134f65d9dee5f41
Valters Vingolds <valters@vingolds.ch>
2016-08-14 12:54:36 +0200

    copyright headers

--- XsDiff/src/main/java/ch/vvingolds/xsdiff/format/SemanticDiffFormatter.java
+++ XsDiff/src/main/java/ch/vvingolds/xsdiff/format/SemanticDiffFormatter.java
@@ -1,3 +1,17 @@
+/*
+  This file is licensed to You under the Apache License, Version 2.0
+  (the "License"); you may not use this file except in compliance with
+  the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 package ch.vvingolds.xsdiff.format;
 
 import java.util.Collection;
@@ -16,6 +30,9 @@ import ch.vvingolds.xsdiff.app.HtmlContentOutput;
 import ch.vvingolds.xsdiff.app.NodeToString;
 import ch.vvingolds.xsdiff.app.XmlDomUtils;
 
+/**
+ * Generates a tabbed view of semantic and other types of diff to view the change.
+ */
 public class SemanticDiffFormatter {
 
     private final XmlDomUtils xmlDomUtils = new XmlDomUtils();
